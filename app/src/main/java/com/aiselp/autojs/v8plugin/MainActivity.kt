c0e60f3df0db172acfa8c3bab40257ee086c4870
aiselp <aiselpk@gmail.com>
2023-09-15 21:36:40 +0800

    添加简单调试工具

--- app/src/main/java/com/aiselp/autojs/v8plugin/MainActivity.kt
+++ app/src/main/java/com/aiselp/autojs/v8plugin/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.aiselp.autojs.v8plugin.ui.theme.V8pluginTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -44,17 +49,39 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MainContext() {
         val code = remember { mutableStateOf("let a = 1 + 2;\nconsole.log(a)") }
-        val out = remember { mutableStateOf("") }
-        Column {
-            Text(text = "测试js代码")
-            TextField(code.value, { code.value = it }, Modifier.fillMaxWidth())
+
+        Column(Modifier.padding(8.dp)) {
+            Card(Modifier.fillMaxWidth()) {
+                Column(Modifier.padding(8.dp)) {
+                    Text(text = "测试js代码:")
+                    TextField(
+                        code.value, { code.value = it },
+                        Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                    )
+                }
+            }
             Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-                Button(onClick = { mainScope.launch { test.execJsString(code.value, out) } }) {
+                Button(onClick = { mainScope.launch { test.execJsString(code.value) } }) {
                     Text(text = "运行")
                 }
             }
-            Text(text = "输出：")
-            TextField(value = out.value, { out.value = it }, Modifier.fillMaxWidth())
+            Card(Modifier.fillMaxWidth()) {
+                Column(Modifier.padding(8.dp).fillMaxSize()) {
+                    Text(text = "输出：")
+                    LazyColumn(
+                        contentPadding = PaddingValues(5.dp)
+                    ) {
+                        test.logList.forEach {
+                            item {
+                                Text(text = it)
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 
--- app/src/main/java/com/aiselp/autojs/v8plugin/test.kt
+++ app/src/main/java/com/aiselp/autojs/v8plugin/test.kt
@@ -1,26 +1,39 @@
 package com.aiselp.autojs.v8plugin
 
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import com.aiselp.autojs.v8engine.V8Engine
 import com.caoccao.javet.annotations.V8Function
+import com.caoccao.javet.values.reference.V8ValueError
+import java.text.SimpleDateFormat
+import java.util.Date
 
 object test {
+    val logList = mutableStateListOf<String>()
+    var dateFormat = SimpleDateFormat("HH:mm:ss.SSS")
 
-    suspend fun execJsString(jsString: String, out: MutableState<String>): Unit {
+    fun execJsString(jsString: String): Unit {
         val engine = V8Engine()
+        logList.clear()
         try {
+            engine.v8Runtime.setPromiseRejectCallback { event, promise, value ->
+                val err = value as V8ValueError
+                val time = dateFormat.format( Date())
+                logList.add("Unhandled Rejection at:', $promise")
+                logList.add("${err.message}:'\nstack: ${err.stack}")
+            }
             engine.v8Runtime.createV8ValueObject().use {
                 it.bind(object {
                     @V8Function
                     fun log(vararg obj: Any?) {
-                        out.value = obj.joinToString(" ")
+                        val time = dateFormat.format( Date())
+                        logList.add("[$time]:${obj.joinToString(" ")}")
                     }
                 })
                 engine.inject("console", it)
             }
             engine.execJsString(jsString)
         } catch (e: Exception) {
-            out.value = e.message.toString()
+            logList.add(e.toString())
         } finally {
             engine.destroy()
         }
